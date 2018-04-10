1b7e9eb3af4df808b4ecfdd6acdfef5d024bdd99
naytheet <naytheet@gmail.com>
2018-04-10 12:10:52 -0700

    Testing: switch from esaude bintray to fswamp.

--- scripts/publish-images.sh
+++ scripts/publish-images.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
 # Log in to the Bintray Docker registry
-docker login -u $BINTRAY_USER -p $BINTRAY_API_KEY esaude-docker-platform-docker.bintray.io
+docker login -u $BINTRAY_USER -p $BINTRAY_API_KEY fswamp-docker-platform-docker.bintray.io
 
 set -o xtrace
 
 # Tag the tomcat image
-docker tag `docker images -q esaude_esaude-platform-tomcat` esaude-docker-platform-docker.bintray.io/tomcat:$TRAVIS_TAG
+docker tag `docker images -q esaude_esaude-platform-tomcat` fswamp-docker-platform-docker.bintray.io/tomcat:$TRAVIS_TAG
 
 # Push the tomcat image
-docker push esaude-docker-platform-docker.bintray.io/tomcat:$TRAVIS_TAG
+docker push fswamp-docker-platform-docker.bintray.io/tomcat:$TRAVIS_TAG
 
 # Tag the mysql image
-docker tag `docker images -q esaude_esaude-platform-mysql` esaude-docker-platform-docker.bintray.io/mysql:$TRAVIS_TAG
+docker tag `docker images -q esaude_esaude-platform-mysql` fswamp-docker-platform-docker.bintray.io/mysql:$TRAVIS_TAG
 
 # Push the mysql image
-docker push esaude-docker-platform-docker.bintray.io/mysql:$TRAVIS_TAG
+docker push fswamp-docker-platform-docker.bintray.io/mysql:$TRAVIS_TAG
 
 # Bundle app for offline install
-docker save -o esaude-app-platform.tar esaude-docker-platform-docker.bintray.io/tomcat esaude-docker-platform-docker.bintray.io/mysql
+docker save -o esaude-app-platform.tar fswamp-docker-platform-docker.bintray.io/tomcat fswamp-docker-platform-docker.bintray.io/mysql
 gzip esaude-app-platform.tar
 
 # Notify to slack
-SLACK_MESSAGE="New eSaude Platform <https://bintray.com/esaude/platform-docker|Docker images> and <https://bintray.com/esaude/apps/esaude-app-platform|offline installer> published ($TRAVIS_TAG)"
-curl -X POST --data-urlencode 'payload={"username": "eSaude Bintray", "text": "'"$SLACK_MESSAGE"'", "icon_url": "https://bintray.com/assets/favicon.png"}' $SLACK_WEBHOOK_URL
+#SLACK_MESSAGE="New eSaude Platform <https://bintray.com/esaude/platform-docker|Docker images> and <https://bintray.com/esaude/apps/esaude-app-platform|offline installer> published ($TRAVIS_TAG)"
+#curl -X POST --data-urlencode 'payload={"username": "eSaude Bintray", "text": "'"$SLACK_MESSAGE"'", "icon_url": "https://bintray.com/assets/favicon.png"}' $SLACK_WEBHOOK_URL
